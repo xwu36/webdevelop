10249904e3a5dc165e05af514be300e4e3cfed14
xwu36 <xwu36@buffalo.edu>
2017-01-14 13:40:41 -0500

    complete signup view, csurf and user model

--- webDevelop/app.js
+++ webDevelop/app.js
@@ -4,10 +4,14 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+var session = require('express-session');
 
 var index = require('./routes/index');
 
 var app = express();
+
+mongoose.connect('localhost:27017/shopping');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -19,6 +23,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(session({secret: 'Ilove33', resave: false, saveUninitialized: false}));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
--- webDevelop/routes/index.js
+++ webDevelop/routes/index.js
@@ -1,9 +1,21 @@
 var express = require('express');
 var router = express.Router();
+var csrf = require('csurf');
+
+var csrfProtection = csrf();
+router.use(csrfProtection);
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.get('/user/signup', function(req, res, next) {
+  res.render('user/signup', {csrfToken: req.csrfToken()});
+});
+
+router.post('/user/signup', function(req, res, next){
+  res.redirect('/');
 });
 
 module.exports = router;
