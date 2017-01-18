c6ee39df4efa416ae2149dfc0f143ca41bee844b
xwu36 <xwu36@buffalo.edu>
2017-01-17 20:49:14 -0500

    complete algorithms list and tackle view

--- webDevelop/app.js
+++ webDevelop/app.js
@@ -9,9 +9,11 @@ var session = require('express-session');
 var passport = require('passport');
 var flash = require('connect-flash');
 var validator = require('express-validator');
+var MongoStore = require('connect-mongo')(session);
 
 var index = require('./routes/index');
 var userRoutes = require('./routes/user');
+var problemsRoutes = require('./routes/problems');
 
 var app = express();
 
@@ -29,19 +31,26 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(validator());
 app.use(cookieParser());
-app.use(session({secret: 'Ilove33', resave: false, saveUninitialized: false}));
+app.use(session({
+  secret: 'Ilove33', 
+  resave: false, 
+  saveUninitialized: false,
+  store: new MongoStore( {mongooseConnection:mongoose.connection} ),
+  cookie: { maxAge: 180 * 60 * 1000 }
+}));
 app.use(flash());
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(function(req, res, next){
-  res.locals.pageNumberGlobal = 1;
+  res.locals.session = req.session;
 	res.locals.login = req.isAuthenticated();
 	next();
 });
 
 app.use('/user', userRoutes);
+app.use('/problems', problemsRoutes);
 app.use('/', index);
 
 // catch 404 and forward to error handler
--- webDevelop/routes/index.js
+++ webDevelop/routes/index.js
@@ -1,42 +1,9 @@
 var express = require('express');
 var router = express.Router();
-var Algorithms = require('../models/algorithms');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
-});
-
-/* GET algorithms page. */
-router.get('/problems/algorithms/:i', function(req, res, next){
-  //res.send('algorithms');
-  Algorithms.find( function(err, docs){
-  	if(err)
-  		redirect('error');
-  	else{
-  		var chunks = [];
-  		var chunkSize = 30;
-  		var pagenumber = (req.params.i - 1) * chunkSize;
-      res.locals.pageNumberGlobal = req.params.i;
-      console.log(res.locals.pageNumberGlobal);
-  		var pages = Math.floor(docs.length/chunkSize) + 1;
-
-  		chunks.push(docs.slice(pagenumber, pagenumber + chunkSize));
-  	  res.render('problems/algorithms', { chunks : chunks, pages : pages, pagenumber : req.params.i });
-    }
-  });
-});
-
-/* GET algorithms page. */
-router.get('/problems/tackle-view/:algorithmsId', function(req, res, next){
-  //res.send('algorithms');
-  var algorithmsId = req.params.algorithmsId;
-  Algorithms.findById(algorithmsId, function(err, docs){
-  	if(err)
-  		res.redirect('error');
-  	else
-  		res.render('problems/tackle-view', { algorithms : docs });
-  });
 });
 
 module.exports = router;
